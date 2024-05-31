8b1fb82fcab2061c202f7c648e17614086a95d33
Chris Done <git@chrisdone.com>
2024-05-31 12:20:51 +0100

    Add carvo

---
+++ webroot/toys/corvo/carvo.js
@@ -0,0 +1,391 @@
+var dark = "#222";
+
+var a_ = [s(1),l(0.02),,,l(0.04),,l(0.02),,,,r(0.01),s(0.5),,r(0.05),,r(0.5),,l(0.05),,s(0.5),,l(0.05),,s(0.1),l(0.1),,l(0.1),,l(0.1),,l(0.05),,l(0.1),,s(0.4),,l(0.05),,l(0.01),,l(0.02),,l(0.5),s(1.6),,s(0.1),l(0.2),,l(0.1),,a(0),,s(2),,];
+
+var b_ = [s(1),l(0.02),,l(0.04),,l(0.05),,,s(7),,l(0.5),s(8.2),,s(1),l(0.44),,,s(0.5),r(0.02),,s(0.3),r(0.2),,r(0.2),,,r(0.06),,r(0.04),,r(0.01),,s(0.5),,,,,,,,l(0.5),a(0),s(4.6),,];
+
+var c_ = [s(1),l(0.03),,l(0.01),,s(1),,l(0.02),,l(0.01),,r(0.01),,,l(0.5),s(2.7),,s(0.2),l(0.1),,l(0.1),,l(0.1),,l(0.1),,,,,a(0),,s(2.7),,];
+
+var d_ = [s(1),l(0.03),,l(0.02),,s(0.5),,l(0.05),,l(0.05),,r(0.07),,r(0.08),,r(0.08),,l(0.5),,l(0.05),,l(0.05),,l(0.07),,l(0.05),,s(0.5),,l(0.2),,l(0.15),,l(0.1),,l(0.018),,s(7.4),,l(0.5),s(7.1),,l(0.05),s(0.3),,l(0.1),,l(0.2),,l(0.04),s(1.8),,];
+
+var e_ = [s(1),l(0.03),,l(0.01),,s(1.5),,s(0.3),l(0.01),,l(0.01),,l(0.04),,l(0.1),,l(0.2),,l(0.1),,l(0.01),,l(0.05),,l(0.05),,l(0.01),,l(0.01),,l(0.01),,,,l(0.01),,,l(0.01),,l(0.01),,l(0.1),,s(0.3),l(0.05),,l(0.1),,l(0.05),,,l(0.02),,a(0),s(2.6),,];
+
+var f_ = [s(1),l(0.03),,l(0.01),,s(1),l(0.05),,l(0.04),,s(2.7),,l(0.5),s(10.55),,l(0.5),s(5.2),,s(2),a(0),,];
+
+var g_ = [s(1),l(0.03),,l(0.01),,s(1),,l(0.05),,s(0.2),r(0.05),,,r(0.01),,l(0.5),,l(0.01),,,,l(0.01),,l(0.02),,s(1),,s(0.2),l(0.2),,l(0.1),,l(0.15),,s(0.5),,l(0.13),,,,l(0.5),r(0.02),s(6.1),,l(0.5),s(4.5),,a(0),s(2.4),,];
+
+var h_ = [s(0.7),l(0.001),,l(0.03),,l(0.08),s(9),,l(0.5),,l(0.45),s(2.5),,r(0.37),s(1.1),,s(0.3),l(0.1),,l(0.2),,a(0),,s(2),,];
+
+var i_ = [s(0.7),l(0.001),,l(0.03),,l(0.03),,l(0.03),,l(0.03),,l(0.01),,,,,l(0.5),s(1.3),,s(0.2),l(0.1),,l(0.1),,l(0.05),,,,l(0.1),,a(0),,s(1.4),,];
+
+var j_ = [s(1),l(0.03),,l(0.01),,s(1),l(0.05),,l(0.04),,s(1.4),,l(0.5),s(9),,l(0.5),s(4.9),,s(2),a(0),,];
+
+var k_ = [s(1),l(0.04),,l(0.03),,l(0.02),,l(0.03),,s(6),,l(0.5),s(6.5),,s(0.1),l(0.2),,l(0.2),,s(0.5),,l(0.05),,l(0.02),,s(1),,l(0.5),s(2),,s(0.2),l(0.1),,l(0.15),,,,,l(0.1),,a(0),,s(2),,];
+
+var l_ = [s(1),l(0.04),,l(0.03),,l(0.02),,l(0.03),,s(6),,l(0.515),s(7.255),,s(0.1),l(0.15),,l(0.2),,s(0.5),,s(2),a(0),,];
+
+var m_ = [s(1),l(0.02),,l(0.03),,l(0.05),,s(2),,s(0.2),r(0.01),,r(0.01),,r(0.01),,r(0.05),,r(0.1),,r(0.1),,,,r(0.2),,,r(0.005),,s(1.6),,l(0.5),s(1.2),,r(0.02),,s(0.1),r(0.1),,,r(0.2),,r(0.15),,s(2),,l(0.05),s(0.1),,l(0.1),,a(0),,s(1.9),,];
+
+var n_ = [s(1),l(0.02),,l(0.03),,l(0.05),,s(1),,s(0.2),r(0.05),, r(0.05),r(0.1),r(0.2),,,r(0.005),,s(1),,l(0.5),s(1),,r(0.02),,s(0.1),r(0.1),,,r(0.2),,r(0.15),,s(1.4),,l(0.05),s(0.1),,l(0.1),,a(0),,s(1.9),,];
+
+var o_ = [s(1),l(0.02),,l(0.03),,l(0.02),,l(0.02),,r(0.01),,s(0.1),,,r(0.2),,r(0.05),,r(0.05),,r(0.05),,r(0.05),,r(0.05),,,s(0.2),,,r(0.05),,r(0.05),,r(0.05),,r(0.05),,r(0.1),,r(0.1),,r(0.05),,r(0.05),,r(0.05),,r(0.01),,,s(1.4),,s(0.2),r(0.1),,r(0.1),,r(0.1),,s(0.7),,s(0.2),l(0.1),,l(0.1),,a(0),s(1),,];
+
+var p_ = [s(1),l(0.01),,l(0.01),s(2.5),,s(0.3),l(0.05),,l(0.05),,l(0.05),,l(0.05),,l(0.1),,l(0.1),,l(0.1),,l(0.1),s(7.8),,l(0.5),s(5),,a(0),s(3),,];
+
+var q_ = [s(1),l(0.02),,l(0.03),,l(0.02),,l(0.025),,,r(0.05),,l(0.5),,l(0.02),,l(0.02),,l(0.05),,l(0.05),,l(0.1),,l(0.2),,,,l(0.1),,l(0.04),s(1.5),,l(0.5),s(7),,l(0.5),s(4.95),,a(0),s(2),,];
+
+var r_ = [s(1),l(0.02),,l(0.05),,l(0.05),,l(0.05),,r(0.2),,l(0.05),,l(0.5),,r(0.05),,l(0.2),,r(0.02),,r(0.02),,r(0.02),,l(0.1),,l(0.2),,l(0.1),a(0),,s(1),,];
+
+var s_ = [s(1),l(0.02),,l(0.01),,l(0.05),,l(0.02),,l(0.02),,s(0.1),l(0.02),,,r(0.4),s(0.4),,r(0.05),,r(0.05),,,r(0.05),,,r(0.05),,,r(0.05),,a(0),,s(2.5),,];
+
+var t_ = [s(1),l(0.02),,l(0.03),,l(0.02),,l(0.025),,l(0.025),,s(4.2),,l(0.5),s(3.4),,l(0.4),s(1.5),,l(0.5),,l(0.1),s(2.5),,s(0.2),l(0.1),,l(0.2),,l(0.1),,a(0),,s(1.3),,];
+
+var u_ = [s(1),l(0.02),,l(0.03),,l(0.02),,l(0.025),,l(0.025),,l(0.53),s(1.3),,s(0.2),l(0.1),,l(0.1),,l(0.1),,l(0.1),,l(0.05),,l(0.05),,s(2),,l(0.5),s(2.2),,a(0),,];
+
+var v_ = [s(1),l(0.02),,l(0.03),,l(0.02),,l(0.025),,,l(0.7),s(1.5),,l(0.4),s(1.6),,s(1.3),r(0.4),,s(0.5),l(0.1),,l(0.1),,a(0),s(0.5),,];
+
+var w_ = [s(1),l(0.02),,l(0.03),,l(0.02),,l(0.025),,,l(0.7),s(1.5),,l(0.4),s(1.5),,s(1.7),r(0.4),,l(0.4),s(1.7),,r(0.35),,s(0.2),l(0.05),,l(0.05),,a(0),s(0.4),,];
+
+var x_ = [s(1),l(0.02),,l(0.05),,l(0.05),,s(0.6),r(0.01),,r(0.1),,r(0.1),,r(0.15),,r(0.15),,r(0.05),,r(0.02),,l(0.55),,s(2),,s(0.2),r(0.02),,r(0.02),,r(0.02),,l(0.55),s(1.3),,s(0.16),l(0.1),,,l(0.1),,,l(0.1),,l(0.05),,,l(0.005),,,,l(0.05),,,l(0.05),,,,a(0),s(1.5),,];
+
+var y_ = [s(1.2),l(0.02),,l(0.05),,l(0.05),,s(0.4),r(0.01),,s(1.3),l(0.5),,s(0.2),l(0.05),,l(0.1),,l(0.1),,,l(0.1),,,l(0.1),,,s(0.5),,s(0.1),l(0.07),s(1.8),,l(0.49),s(7),,l(0.5),s(4.35),,a(0),s(2),,];
+
+var alphabet = [a_, b_, c_, d_, e_, f_, g_, h_, i_, j_, k_, l_, m_, n_, o_, p_, q_, r_, s_, t_, u_, v_, w_, x_, y_];
+
+var horizontalLines = {
+  ascenderHeight: 0.1,
+  capHeight: 0.125,
+  median: 0.35,
+  baseline: { value: 0.42 },
+  descenderHeight: 0.575
+};
+
+var verticalLines = {
+  leftMargin: 0.1,
+  lead: 0.15,
+  origin: { value: 0.175 },
+  advance: 0.25,
+  trail: 0.275
+};
+
+var dimensions = {
+  width: 1024,
+  height: 300
+};
+
+var ctx, state;
+
+function clear(){
+  ctx.clearRect(0,0,dimensions.width,dimensions.height);
+}
+
+function main(){
+  var canvas = document.getElementById('canvas');
+  if (canvas.getContext) {
+    ctx = canvas.getContext('2d');
+    state = newstate();
+    // lines(ctx,dimensions.width,dimensions.height);
+    var chunks = poem.split(/ /);
+    forloop(500,0,function(){return chunks.length},1,function(i,done){
+      letters(chunks[i],done);
+    },function(){
+    });
+  }
+}
+
+function letters(string,done){
+  var els = [];
+  for (var i = 0, len = string.length; i < len; i++) {
+    if (string[i] == '\n') {
+      els.push([cr,lf]);
+    } else {
+      els.push(alphabet[string.charCodeAt(i) - 97]);
+    }
+  }
+  turtle(ctx,state,[].concat.apply([],els),done);
+}
+
+function newstate(){
+  var state = new State(verticalLines.lead*dimensions.width,horizontalLines.baseline.value*dimensions.height,0,0,0,0);
+  state.velocity = 1;
+  state.resistance = 0.01;
+  return state;
+}
+
+// Draw the glyph guides
+function lines(ctx,xscale,yscale){
+  var old = ctx.strokeStyle;
+  ctx.strokeStyle = "#cccccc";
+  var lw = ctx.lineWidth;
+  for (var line in horizontalLines) {
+    ctx.beginPath();
+    var l = horizontalLines[line];
+    if (horizontalLines[line].value) {
+      l = l.value;
+      ctx.strokeStyle = dark;
+    } else {
+      ctx.strokeStyle = "#cccccc";
+    }
+    ctx.moveTo(0,l*yscale);
+    ctx.lineTo(xscale,l*yscale);
+    ctx.stroke();
+  }
+  for (var line in verticalLines) {
+    ctx.beginPath();
+    var l = verticalLines[line];
+    if (verticalLines[line].value) {
+      l = l.value;
+      ctx.strokeStyle = dark;
+    } else {
+      ctx.strokeStyle = "#cccccc";
+    }
+    ctx.moveTo(xscale*l,0);
+    ctx.lineTo(xscale*l,yscale);
+    ctx.stroke();
+  }
+
+  ctx.strokeStyle = old;
+}
+
+function turtle(ctx,state,steps,done){
+  ctx.clearRect(0,0,dimensions.width,dimensions.height);
+  state = newstate();
+  // lines(ctx,dimensions.width,dimensions.height);
+  var len = steps.length;
+  forloop(1,0,function(){return len},1,function(i,cont){
+    if(steps[i]) {
+      if (!steps[i](state,ctx))
+        animate(ctx,state);
+      cont();
+    }
+    else {
+      for (var x = 0; x < state.velocity; x += 0.1){
+        animate(ctx,state);
+      }
+      cont();
+    }
+  },done);
+}
+
+function forloop(ms,i,to,inc,body,done){
+  if (i < to()) {
+    body(i,function(){
+      setTimeout(function(){
+        forloop(ms,i+inc,to,inc,body,done);
+      },ms);
+    });
+  } else {
+    done();
+  }
+}
+
+function animate(ctx,state){
+  draw(ctx,state);
+  update(state);
+}
+
+function update(state){
+  state.velocity = Math.max(0,state.velocity-state.resistance);
+  state.x = state.x + (Math.cos(state.radian) * 1);
+  state.y = state.y + (Math.sin(state.radian) * 1);
+  state.pressure = (state.pressure + (Math.max(0.01,0.5 * (1 - state.velocity)))) / 2;
+}
+
+function cr(){
+  state.x = verticalLines.lead*dimensions.width;
+  return true;
+}
+
+function lf(){
+  state.y = 50;
+  return true;
+}
+
+function a(ratio){
+  return function(state){
+    state.radian = ratio * (Math.PI * 2);
+    return true;
+  }
+}
+
+function l(ratio){
+  return function(state){
+    state.radian = state.radian - (ratio * (Math.PI * 2));
+  }
+}
+
+function r(ratio){
+  return function(state){
+    state.radian = state.radian + (ratio * (Math.PI * 2));
+  }
+}
+
+function s(speed){
+  return function(state){
+    state.velocity = speed;
+  }
+}
+
+function c(color){
+  return function(state,ctx){
+    // ctx.fillStyle = color;
+    return false;
+  }
+}
+
+function draw(ctx,state){
+  ctx.beginPath();
+  ctx.arc(state.x,state.y,0.4 + state.pressure,0,Math.PI*2);
+  ctx.fill();
+}
+
+function State(x,y,ra,v,r,p){
+  this.x = x;
+  this.y = y;
+  this.radian = ra;
+  this.velocity = v;
+  this.resistance = r;
+  this.pressure = p;
+}
+
+function randomIntFromInterval(min,max) {
+  return Math.floor(Math.random()*(max-min+1)+min);
+}
+
+var poem =
+["once upon a midnight dreary, while i pondered, weak and weary,",
+"over many a quaint and curious volume of forgotten lore—",
+"while i nodded, nearly napping, suddenly there came a tapping,",
+"as of some one gently rapping, rapping at my chamber door—",
+"”‘tis some visitor,” i muttered, “tapping at my chamber door—",
+"               only this and nothing more.”",
+"",
+"ah, distinctly i remember it was in the bleak december;",
+"and each separate dying ember wrought its ghost upon the floor.",
+"eagerly i wished the morrow;—vainly i had sought to borrow",
+"from my books surcease of sorrow—sorrow for the lost lenore—",
+"for the rare and radiant maiden whom the angels name lenore—",
+"               nameless here for evermore.",
+"",
+"and the silken, sad, uncertain rustling of each purple curtain",
+"thrilled me—filled me with fantastic terrors never felt before;",
+"so that now, to still the beating of my heart, i stood repeating,",
+"”‘tis some visitor entreating entrance at my chamber door—",
+"some late visitor entreating entrance at my chamber door;—",
+"               this it is and nothing more.”",
+"",
+"presently my soul grew stronger; hesitating then no longer,",
+"“sir,” said i, “or madam, truly your forgiveness i implore;",
+"but the fact is i was napping, and so gently you came rapping,",
+"and so faintly you came tapping, tapping at my chamber door,",
+"that i scarce was sure i heard you”—here i opened wide the door;—",
+"               darkness there and nothing more.",
+"",
+"deep into that darkness peering, long i stood there wondering, fearing,",
+"doubting, dreaming dreams no mortal ever dared to dream before;",
+"but the silence was unbroken, and the stillness gave no token,",
+"and the only word there spoken was the whispered word, “lenore?”",
+"this i whispered, and an echo murmured back the word, “lenore!”—",
+"               merely this and nothing more.",
+"",
+"back into the chamber turning, all my soul within me burning,",
+"soon again i heard a tapping somewhat louder than before.",
+"“surely,” said i, “surely that is something at my window lattice;",
+"let me see, then, what thereat is, and this mystery explore—",
+"let my heart be still a moment and this mystery explore;—",
+"               ‘tis the wind and nothing more!”",
+"",
+"open here i flung the shutter, when, with many a flirt and flutter,",
+"in there stepped a stately raven of the saintly days of yore;",
+"not the least obeisance made he; not a minute stopped or stayed he;",
+"but, with mien of lord or lady, perched above my chamber door—",
+"perched upon a bust of pallas just above my chamber door—",
+"                perched, and sat, and nothing more.",
+"",
+"then this ebony bird beguiling my sad fancy into smiling,",
+"by the grave and stern decorum of the countenance it wore,",
+"“though thy crest be shorn and shaven, thou,” i said, “art sure no craven,",
+"ghastly grim and ancient raven wandering from the nightly shore—",
+"tell me what thy lordly name is on the night’s plutonian shore!”",
+"               quoth the raven “nevermore.”",
+"",
+"much i marvelled this ungainly fowl to hear discourse so plainly,",
+"though its answer little meaning—little relevancy bore;",
+"for we cannot help agreeing that no living human being",
+"ever yet was blest with seeing bird above his chamber door—",
+"bird or beast upon the sculptured bust above his chamber door,",
+"               with such name as “nevermore.”",
+"",
+"but the raven, sitting lonely on the placid bust, spoke only",
+"that one word, as if his soul in that one word he did outpour.",
+"nothing further then he uttered—not a feather then he fluttered—",
+"till i scarcely more than muttered “other friends have flown before—",
+"on the morrow he will leave me, as my hopes have flown before.”",
+"               then the bird said “nevermore.”",
+"",
+"startled at the stillness broken by reply so aptly spoken,",
+"“doubtless,” said i, “what it utters is its only stock and store",
+"caught from some unhappy master whom unmerciful disaster",
+"followed fast and followed faster till his songs one burden bore—",
+"till the dirges of his hope that melancholy burden bore",
+"               of ‘never—nevermore.'”",
+"",
+"but the raven still beguiling my sad fancy into smiling,",
+"straight i wheeled a cushioned seat in front of bird, and bust and door;",
+"then, upon the velvet sinking, i betook myself to linking",
+"fancy unto fancy, thinking what this ominous bird of yore—",
+"what this grim, ungainly, ghastly, gaunt and ominous bird of yore",
+"               meant in croaking “nevermore.”",
+"",
+"this i sat engaged in guessing, but no syllable expressing",
+"to the fowl whose fiery eyes now burned into my bosom’s core;",
+"this and more i sat divining, with my head at ease reclining",
+"on the cushion’s velvet lining that the lamp-light gloated o’er,",
+"but whose velvet violet lining with the lamp-light gloating o’er,",
+"               she shall press, ah, nevermore!",
+"",
+"then, methought, the air grew denser, perfumed from an unseen censer",
+"swung by seraphim whose foot-falls tinkled on the tufted floor.",
+"“wretch,” i cried, “thy god hath lent thee—by these angels he hath sent thee",
+"respite—respite and nepenthe, from thy memories of lenore;",
+"quaff, oh quaff this kind nepenthe and forget this lost lenore!”",
+"               quoth the raven “nevermore.”",
+"",
+"“prophet!” said i, “thing of evil!—prophet still, if bird or devil!—",
+"whether tempter sent, or whether tempest tossed thee here ashore,",
+"desolate yet all undaunted, on this desert land enchanted—",
+"on this home by horror haunted—tell me truly, i implore—",
+"is there—is there balm in gilead?—tell me—tell me, i implore!”",
+"               quoth the raven “nevermore.”",
+"",
+"“prophet!” said i, “thing of evil—prophet still, if bird or devil!",
+"by that heaven that bends above us—by that god we both adore—",
+"tell this soul with sorrow laden if, within the distant aidenn,",
+"it shall clasp a sainted maiden whom the angels name lenore—",
+"clasp a rare and radiant maiden whom the angels name lenore.”",
+"                quoth the raven “nevermore.”",
+"",
+"“be that word our sign in parting, bird or fiend!” i shrieked, upstarting—",
+"“get thee back into the tempest and the night’s plutonian shore!",
+"leave no black plume as a token of that lie thy soul hath spoken!",
+"leave my loneliness unbroken!—quit the bust above my door!",
+"take thy beak from out my heart, and take thy form from off my door!”",
+"               quoth the raven “nevermore.”",
+"",
+"and the raven, never flitting, still is sitting, still is sitting",
+"on the pallid bust of pallas just above my chamber door;",
+"and his eyes have all the seeming of a demon’s that is dreaming,",
+"and the lamp-light o’er him streaming throws his shadow on the floor;",
+"and my soul from out that shadow that lies floating on the floor",
+"               shall be lifted—nevermore!"].join(" ").replace(/[^a-z ]/,'');
+
+/**
+* Split a string into chunks of the given size
+* @param {String} string is the String to split
+* @param {Number} size is the size you of the cuts
+* @return {Array} an Array with the strings
+*/
+function splitString (string, size) {
+var re = new RegExp('.{1,' + size + '}', 'g');
+return string.match(re);
+}
